81cdec56acf03308300fdcbe973954d37ccc37ea
Nello Casolla <nellocasolla446@gmail.com>
2023-03-29 17:53:25 +0200

    final

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,7 +1,8 @@
 import { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
-import books from "../data/horror.json";
+import horror from "../data/horror.json";
+// import romance from "../data/romance.json";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Container } from "react-bootstrap";
@@ -11,7 +12,7 @@ class AllTheBooks extends Component {
     return (
       <Container>
         <Row md={4}>
-          {books.map((book, index) => (
+          {horror.map((book, index) => (
             <Col key={index}>
               <Card className="d-flex">
                 <Card.Body>
@@ -27,6 +28,25 @@ class AllTheBooks extends Component {
           ))}
           ;
         </Row>
+        {/* <Row md={4}>
+          <h3>Romace books: </h3>
+          <br />
+          {romance.map((book, index) => (
+            <Col key={index}>
+              <Card className="d-flex">
+                <Card.Body>
+                  <Card.Img variant="bottom" src={book.img} alt="First slide" />
+
+                  <h3>{book.title}</h3>
+                  <p>
+                    <Badge bg="dark">{book.price}€</Badge>
+                  </p>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+          ;
+        </Row> */}
       </Container>
     );
   }
